0a16c301f25d22f6ea0c33a57c3b61e06ab5a99d
benniejung <ybe1027@hansung.ac.kr>
2026-08-09 00:07:15 +0900

    fix: S3가 Records 타입 인식못하는 오류 해결

--- src/main/java/com/yebin/sideproject/domain/track/dto/S3EventNotificationDto.java
+++ src/main/java/com/yebin/sideproject/domain/track/dto/S3EventNotificationDto.java
@@ -2,10 +2,12 @@ package com.yebin.sideproject.domain.track.dto;
 
 // S3에서 저장됐다고 알릴때 보내는 정보들
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import java.util.List;
 
 public record S3EventNotificationDto (
-        List<Record> records
+        @JsonProperty("Records") List<Record> records
 ) {
     public record Record(
             String eventName,
