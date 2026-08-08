cefdc62d25e019b760316ec9adcc5f5f2da27786
benniejung <ybe1027@hansung.ac.kr>
2026-08-08 18:35:18 +0900

    feat: s3로부터 응답 받는 데이터 S3EventNotificationDto 파일 추가

---
+++ src/main/java/com/yebin/sideproject/domain/track/dto/S3EventNotificationDto.java
@@ -0,0 +1,28 @@
+package com.yebin.sideproject.domain.track.dto;
+
+// S3에서 저장됐다고 알릴때 보내는 정보들
+
+import java.util.List;
+
+public record S3EventNotificationDto (
+        List<Record> records
+) {
+    public record Record(
+            String eventName,
+            S3Info s3
+    ) {}
+
+    public record S3Info(
+            Bucket bucket,
+            S3Object object
+    ) {}
+
+    public record Bucket(String name) {}
+
+    public record S3Object(
+            String key,
+            long size,
+            String eTag,
+            String contentType
+    ) {}
+}
