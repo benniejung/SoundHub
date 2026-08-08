123d8dba2e83748abebb4ef854195ff93a698189
benniejung <ybe1027@hansung.ac.kr>
2026-08-08 23:05:28 +0900

    feat: S3웹훅 처리 후 접근하는 Controller 추가

--- src/main/java/com/yebin/sideproject/domain/track/controller/S3EventWebhookController.java
+++ src/main/java/com/yebin/sideproject/domain/track/controller/S3EventWebhookController.java
@@ -1,9 +1,26 @@
 package com.yebin.sideproject.domain.track.controller;
 
+import com.yebin.sideproject.domain.track.dto.S3EventNotificationDto;
+import com.yebin.sideproject.domain.track.service.TrackService;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequiredArgsConstructor
 @RequestMapping("/api/webhooks/s3-events")
 public class S3EventWebhookController {
+    private final TrackService trackService;
+
+    @PostMapping
+    public ResponseEntity<Void> handleWebhook(@RequestBody S3EventNotificationDto dto) {
+        for (S3EventNotificationDto.Record record : dto.records()) {
+            trackService.handleS3UploadComplete(record.s3().object());
+        }
+        return ResponseEntity.ok().build(); // 전달하는 바디 없이 성공만 알릴때
+    }
 }
