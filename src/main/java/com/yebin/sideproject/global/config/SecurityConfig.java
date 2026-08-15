2afa74ec6dca5eeeeac90d1881eb7149b3a7b1bc
benniejung <ybe1027@hansung.ac.kr>
2026-08-16 00:28:40 +0900

    chore: Grafana + Prometheus 설정 추가

--- src/main/java/com/yebin/sideproject/global/config/SecurityConfig.java
+++ src/main/java/com/yebin/sideproject/global/config/SecurityConfig.java
@@ -37,6 +37,7 @@ public class SecurityConfig {
                         .requestMatchers("/api/auth/**", "/error").permitAll()
                         .requestMatchers("/swagger-ui/**", "/swagger-ui.html", "/v3/api-docs/**").permitAll()
                         .requestMatchers("/api/webhooks/s3-events").permitAll()
+                        .requestMatchers("/actuator/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 // jsonFilter가 UsernamePasswordAuthenticationFilter 자리를 대체
